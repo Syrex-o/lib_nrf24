907dece297adcfaf72009be658946ed0d23f9e9c
Syrex-o <alberthenkel.ah@gmail.com>
2019-07-11 10:13:25 +0200

    Update receive.py

--- receive.py
+++ receive.py
@@ -12,7 +12,8 @@ GPIO.setwarnings(False)
 PINS = [2,3,4,5,6,7]
 
 # initializing GPIOS to HIGH
-for i in PINS: GPIO.setup(i, GPIO.OUT, initial=GPIO.HIGH)
+for i in PINS:
+    GPIO.setup(i, GPIO.OUT, initial=GPIO.HIGH)
 
 pipes = [[0xe7, 0xe7, 0xe7, 0xe7, 0xe7], [0xc2, 0xc2, 0xc2, 0xc2, 0xc2]]
 
@@ -23,7 +24,7 @@ radio.begin(0, 17)
 radio.setRetries(15,15)
 
 radio.setPayloadSize(8)
-radio.setChannel(0x60)
+radio.setChannel(124)
 radio.setDataRate(NRF24.BR_250KBPS)
 radio.setPALevel(NRF24.PA_MAX)
 
@@ -45,35 +46,35 @@ isTimerActive = False
 
 # setting pin off
 def pinOff(pin):
-	global isTimerActive
-	GPIO.output(pin, GPIO.HIGH)
-	isTimerActive = False
+    global isTimerActive
+    GPIO.output(pin, GPIO.HIGH)
+    isTimerActive = False
 
 # listening loop
 while True:
-	while not radio.available(0):
-		time.sleep(1/100)
-	# message income
-	# writing payload
-	radio.writeAckPayload(1, ackPL, len(ackPL))
-	receivedMessage = []
-	radio.read(receivedMessage, radio.getDynamicPayloadSize())
-	# translating message
-	arr = []
-	for n in receivedMessage:
-		# Decode into standard unicode set
-		if (n >= 32 and n <= 126):
-			arr.append(chr(n))
-	# setting pins off
+    while not radio.available(0):
+        time.sleep(1/100)
+    # message income
+    # writing payload
+    radio.writeAckPayload(1, ackPL, len(ackPL))
+    receivedMessage = []
+    radio.read(receivedMessage, radio.getDynamicPayloadSize())
+    # translating message
+    arr = []
+    for n in receivedMessage:
+        # Decode into standard unicode set
+        if (n >= 32 and n <= 126):
+            arr.append(chr(n))
+    # setting pins off
     for pin in PINS:
-    	if not GPIO.input(pin):
-    		GPIO.output(pin, GPIO.HIGH)
+        if not GPIO.input(pin):
+            GPIO.output(pin, GPIO.HIGH)
     # setting correct GPIO
     if arr[1] == '1':
-    	GPIO.output(int(arr[0]), GPIO.LOW)
-    	secs = int(arr[2:])
-    	# timer
-    	if isTimerActive:
+        GPIO.output(int(arr[0]), GPIO.LOW)
+        secs = int("".join(arr)[2:])
+        # timer
+        if isTimerActive:
             t.cancel()
             isTimerActive = False
         t = Timer(secs, pinOff, [int(arr[0])])
